00f7f1a92c884402dd97159ea8e0208188ad620d
Ayesha Ghoshal <ghoshalayesha@gmail.com>
2016-03-04 13:37:02 +0530

    Added files via upload

---
+++ ex2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hell again"
+print "I like typing this"
+print "This is fun"
+print "Yay! printing"
+print "I'd much rather you 'not"
+print "Another line to print"
+
+# A comment, this is so you can read it at a later stage
+print "I could code like this"
+# You can use a comment to 'disable' or comment out a piece of code
+# print "This won't run"
+print "This will run"
+
+# New comment: Counting stock in the farm!
+
+print "I will now count my chickens:"
+# Description: Simple division & then addition
+print "Hens", 25 + 30 / 6
+#Description: Multiplication and then Modulo of 75 % 4 = 3, which gives 100 - 3 as the answer
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs"
+# Perform Modulo and division. Actual value = 6.75 but final value has been rounded up 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# < function asks the question of True or False
+print 3 + 2 < 5 - 7
+# Simple addition & subtraction functions. The following condtions axplain why the < equation was false
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that is why it's False"
+
+print "How about some more."
+# Determines the trueness of the equation. Whether the question is True or False
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
